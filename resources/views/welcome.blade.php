20cc49752bacafc92aa573ded13d52d0fdbecde1
unknown <to.tanwa@gmail.com>
2018-06-01 11:11:29 +0700

    first 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,12 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
         <title>Laravel</title>
 
     </head>
     <body>
-        <input id='message' type="text">
+        <input id='message' type="text" onkeyup="update();">
         <h1 id="h1"></h1>
     </body>
     <script>
